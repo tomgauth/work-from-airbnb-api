f6729154da7b6de3c461a0808a84a150ce06a771
tom gauthier <tomgauthier@MacBook-Pro-de-tom.local>
2022-01-27 18:19:25 +0100

    heroku set up

--- tests/scraper_tests.py
+++ tests/scraper_tests.py
@@ -1,7 +1,6 @@
 import pytest
 from services.airbnb_img_scraper import ImagesScraper
 
-# make sure the scraper returns a list
 
 def test_airbnb_img_scraper():
 	image_scraper = ImagesScraper(listing_url="")
@@ -12,4 +11,3 @@ def test_airbnb_img_scraper_is_list():
 	image_scraper = ImagesScraper(listing_url="https://www.airbnb.fr/rooms/669432")
 	assert type(image_scraper.scrape_images()) is list
 
-# make sure that all elements in list are urls
