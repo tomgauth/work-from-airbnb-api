8b652f69426c68d954137050c415eda45b7fc25d
tom gauthier <tomgauthier@MacBook-Pro-de-tom.local>
2022-01-27 18:46:57 +0100

    added waitress

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 configure_routes(app)
 
-if __name__ == '__main__':    
+if __name__ == '__main__':
+    from waitress import serve 
     port = int(os.environ.get('PORT', 5000))
     app.run(host = '0.0.0.0', port = port)
